1619a23beb0d32c9e13e81be051be4ee3f1f3e89
Asep Riyandi <70046152+riyand0902@users.noreply.github.com>
2023-11-24 09:24:44 +0700

    Add files via upload

---
+++ nodejs-web-server/OV5640_Console_Test/OV5640_Console_Test.ino
@@ -0,0 +1,130 @@
+#include "esp_camera.h"
+#include "ESP32_OV5640_AF.h"
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+
+const char* ssid ="Rahman_99";
+const char* password ="illiyiin@99";
+const char* websocket_server_host = "192.168.12.108";
+const uint16_t websocket_server_port = 8888;
+
+
+using namespace websockets;
+WebsocketsClient client;
+
+// ESP32 AI-THINKER Board 
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+OV5640 ov5640 = OV5640();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t* sensor = esp_camera_sensor_get();
+  ov5640.start(sensor);
+
+  if (ov5640.focusInit() == 0) {
+    Serial.println("OV5640_Focus_Init Successful!");
+  }
+
+  if (ov5640.autoFocusMode() == 0) {
+    Serial.println("OV5640_Auto_Focus Successful!");
+  }
+
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi Connected");
+
+  Serial.print("use link : 'http://");
+  Serial.print(WiFi.localIP());
+
+  while(!client.connect(websocket_server_host, websocket_server_port, "/")){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WebSocket Connected!");
+}
+
+void loop() {
+  camera_fb_t* fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    esp_camera_fb_return(fb);
+    return;
+  }
+
+  uint8_t rc = ov5640.getFWStatus();
+  Serial.printf("FW_STATUS = 0x%x\n", rc);
+
+  if (rc == -1) {
+    Serial.println("Check your OV5640");
+  } else if (rc == FW_STATUS_S_FOCUSED) {
+    Serial.println("Focused!");
+  } else if (rc == FW_STATUS_S_FOCUSING) {
+    Serial.println("Focusing!");
+  } else {
+  }
+
+  if (fb->format != PIXFORMAT_JPEG) {
+    Serial.println("Non-JPEG data not implemented");
+    esp_camera_fb_return(fb);
+    return;
+  }
+
+  client.sendBinary((const char*) fb->buf, fb->len);
+  esp_camera_fb_return(fb);
+}
---
+++ nodejs-web-server/OV5640_Console_Test/server.js
@@ -0,0 +1,26 @@
+const path = require(`path`);
+const express = require('express');
+const WebSocket = require('ws');
+const app = express();
+
+const WS_PORT = 8888;
+const HTTP_PORT = 8000;
+
+const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
+
+let connectedClients = [];
+wsServer.on('connection', (ws,req)=>{
+    console.log('connected');
+    connectedClients.push(ws);
+
+    ws.on('message', data => {
+        if(ws.readyState === ws.OPEN){
+            ws.send(data);
+        }else{
+            connectedClients.splice(i, 1);
+        }
+    });
+});
+
+app.get(`/client`, (req,res)=>res.sendFile(path.resolve(__dirname, `./client.html`)));
+app.listen(HTTP_PORT, ()=> console.log(`HTTP Server Listening at ${HTTP_PORT}`));
